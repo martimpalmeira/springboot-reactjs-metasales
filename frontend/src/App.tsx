6eec1936ca2c0513ef374a2b573b325a99666d05
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-07-15 19:58:26 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,16 @@
+import NotificationButton from "./components/NotificationButton"
 
 
 function App() {
   return (
-    <h1>Olá mundo!</h1>
+    <>
+      <h1>Olá mundo!</h1>
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+      <NotificationButton />
+    </>
   )
 }
 
