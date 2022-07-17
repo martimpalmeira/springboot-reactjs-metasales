1e7b075b47fc7266e074adcc90b5355da8aec87b
martimpalmeira <martim.desenvolvedor@gmail.com>
2022-07-17 17:02:10 -0300

    Final

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -6,9 +6,9 @@ function Header() {
         <header>
             <div className="dsmeta-logo-container">
                 <img src={logo} alt="DSMeta" />
-                <h1>DSMeta</h1>
+                <h1>Meta Sales</h1>
                 <p>
-                    Desenvolvido por
+                    Developed by
                     <a href="https://www.linkedin.com/in/martimjunior/" target="_blank"> @martimpalmeira</a>
                 </p>
             </div>
